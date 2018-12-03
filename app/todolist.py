bd38cc2018538d22157c1f423376d6e8cd8c8de7
jl <jonathan.arthur.lock@gmail.com>
2018-12-03 13:07:48 +0100

    added priortiy

--- app/todolist.py
+++ app/todolist.py
@@ -1,15 +1,190 @@
-# connect to db
+import sqlite3
+import os
+from datetime import datetime
+import tabulate
 
 
-# class TODO:
-# init
+def connect(filename):
+    is_existing = os.path.exists(filename)
+    db = sqlite3.connect("todolist.db")
+    cursor = db.cursor()
+    if not is_existing:
+        cursor.execute(
+            "CREATE TABLE Task ("
+            "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+            "Name TEXT NOT NULL,"
+            "Details TEXT NOT NULL,"
+            "Priority INTEGER NOT NULL,"
+            "Status TEXT NOT NULL,"
+            "Start DATETIME,"
+            "Finish DATETIME,"
+            "Duration DATETIME);"
+        )
+    return db
 
-# create
-# doing
-# done
-# edit
-# delete
 
-# list all
-# list doing
-# list x priority
+class Todo:
+    def __init__(self):
+        self.db = connect("todolist.db")
+        self.cursor = self.db.cursor()
+
+    def todo(self, task_name, task_details, task_priority):
+        self.cursor.execute(
+            "INSERT INTO Task(Name, Details, Status, Priority)VALUES(?,?,?,?)",
+            (task_name, task_details, "Todo", task_priority),
+        )
+        self.db.commit()
+        return f"'{task_name.title()}' has been created"
+
+    def doing(self, task_id):
+        self.cursor.execute(f"SELECT Status FROM Task WHERE ID = {task_id}")
+        results = self.cursor.fetchone()
+        if results:
+            status = results[0]
+            if status == "Todo":
+                try:
+                    self.cursor.execute(
+                        "UPDATE Task SET Status ='Doing',Start='{0}' WHERE ID = {1}".format(
+                            datetime.now().strftime("%Y-%m-%d %H:%M:%S"), task_id
+                        )
+                    )
+                    self.db.commit()
+                    return f"Task {task_id} has been updated"
+                except sqlite3.OperationalError:
+                    print("Invalid task ID!")
+
+            elif status == "Doing":
+                return "You are already doing this task"
+
+            elif status == "Done":
+                try:
+                    self.cursor.execute(
+                        "UPDATE Task SET Status ='Doing',Start='{0}', Finish='NULL' WHERE ID = {1}".format(
+                            datetime.now().strftime("%Y-%m-%d %H:%M:%S"), task_id
+                        )
+                    )
+                    self.db.commit()
+                    return f"Task with ID {task_id} has been successfully updated!"
+                except sqlite3.OperationalError as err:
+                    return f"{err}"
+            return "Invalid task status"
+        return "Invalid task status"
+
+    def done(self, task_id):
+        self.cursor.execute(f"SELECT Status FROM Task WHERE ID = {task_id}")
+        results = self.cursor.fetchone()
+        if results:
+            status = results[0]
+            if status == "Doing":
+                try:
+                    self.cursor.execute(f"SELECT Start FROM Task WHERE ID={task_id}")
+                    start_time = datetime.strptime(
+                        self.cursor.fetchone()[0], "%Y-%m-%d %H:%M:%S"
+                    )
+                    finish_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    duration = (
+                        datetime.strptime(finish_time, "%Y-%m-%d %H:%M:%S") - start_time
+                    )
+                    self.cursor.execute(
+                        "UPDATE Task SET Status='Done', Finish='{0}', Duration='{1}'  WHERE ID = {2}".format(
+                            finish_time, duration, task_id
+                        )
+                    )
+                    self.db.commit()
+                    return f"Task with ID {task_id} has successfully been completed!"
+                except sqlite3.OperationalError:
+                    return "unknown task ID"
+            if status == "Done":
+                return "Task has already been completed"
+            return "The task specified is still on the todo list: try changing it's status first"
+        return "Invalid task status"
+
+    def list_all(self):
+        self.cursor.execute("SELECT ID, Name, Details, Status, Priority FROM Task")
+        results = self.cursor.fetchall()
+        if results:
+            headers = ["ID", "Name", "Details", "Status", "Priority"]
+            return tabulate.tabulate(results, headers, tablefmt="fancy_grid")
+        return "No tasks added yet! please add some tasks and try again"
+
+    def list_todo(self):
+        self.cursor.execute(
+            "SELECT ID, Name, Details, Priority FROM Task WHERE Status='Todo'"
+        )
+        results = self.cursor.fetchall()
+        if results:
+            headers = ["ID", "Name", "Details", "Priority"]
+            return tabulate.tabulate(results, headers, tablefmt="fancy_grid")
+        return "No tasks pending"
+
+    def list_doing(self):
+        self.cursor.execute(
+            "SELECT ID, Name, Details, Priority, Start FROM Task WHERE Status='Doing'"
+        )
+        results = self.cursor.fetchall()
+        if results:
+            headers = ["ID", "Name", "Details", "Priority", "Start Time"]
+            return tabulate.tabulate(results, headers, tablefmt="fancy_grid")
+        return "No tasks are being worked on currently"
+
+    def list_done(self):
+        self.cursor.execute(
+            "SELECT ID, Name, Details, Priority, Start, Finish, Duration FROM Task WHERE Status='Done'"
+        )
+        results = self.cursor.fetchall()
+        if results:
+            headers = [
+                "ID",
+                "Name",
+                "Details",
+                "Priority",
+                "Started on",
+                "Finished on",
+                "Duration",
+            ]
+            return tabulate.tabulate(results, headers, tablefmt="fancy_grid")
+        return "No tasks are being worked on currently!"
+
+    def list_priority(self, priority_level):
+        self.cursor.execute(
+            f"SELECT ID, Name, Details, Priority, Start FROM Task WHERE Priority = {priority_level}"
+            )
+        results = self.cursor.fetchall()
+        if results:
+            headers = [
+                "ID",
+                "Name",
+                "Details",
+                "Priority",
+                "Started on",
+                "Duration",
+            ]
+            return tabulate.tabulate(results, headers, tablefmt="fancy_grid")
+        return "No tasks matching this priority level"
+
+    def edit_task(self, task_id):
+        self.cursor.execute(f"SELECT Name, Details FROM Task WHERE ID={task_id}")
+        results = self.cursor.fetchone()
+        if results:
+            new_name = input("Enter task name: ")
+            new_details = input("Task details: ")
+            new_priority = input("Task priority: ")
+            if new_details and new_name and new_priority:
+                self.cursor.execute(
+                    'UPDATE Task SET Name = \'{}\', Details = "{}", Priorty = "{}", WHERE ID = {}'.format(
+                        new_name, new_details, new_priority, task_id
+                    )
+                )
+                self.db.commit()
+                return "Task has been edited successfully!!"
+            return "A task name is required"
+        return "Task not found!"
+
+    def delete_task(self, task_id):
+        self.cursor.execute(f"SELECT Name FROM Task WHERE ID = {task_id}")
+        result = self.cursor.fetchone()
+        if result:
+            self.cursor.execute(f"DELETE FROM Task WHERE ID = {task_id}")
+            self.db.commit()
+            return f"Task to '{result[0]}' with ID '{task_id}' has been deleted"
+        return "Task not found!"
--- main.py
+++ main.py
@@ -8,15 +8,17 @@ Usage:
     todolist list_todo
     todolist list_doing
     todolist list_done
-    todolist list_priority
+    todolist list_priority <priority_level>
     todolist list_id
+
     todolist quit
 Options:
   -h --help     Show these commands.
 """
+
 import os
 import cmd
-import docopt
+from docopt import DocoptExit, docopt
 from app.todolist import Todo
 
 
@@ -26,15 +28,16 @@ def docopt_cmd(func):
     This decorator is used to simplify the try/except block and pass the result
     of the docopt parsing to the called action.
     """
+
     def fn(self, arg):
         try:
             opt = docopt(fn.__doc__, arg)
 
-        except DocoptExit as err:
+        except DocoptExit as e:
             # The DocoptExit is thrown when the args do not match
             # We print a message to the user and the usage block
-            print('Invalid Command!')
-            print(err)
+            print("Invalid Command!")
+            print(e)
             return
 
         except SystemExit:
@@ -51,8 +54,8 @@ def docopt_cmd(func):
 
 
 class Main(cmd.Cmd):
-    os.system('clear')
-    prompt = "Todo >>"
+    os.system("clear")
+    prompt = "Todo >>>"
 
     def __init__(self):
         super().__init__()
@@ -60,52 +63,65 @@ class Main(cmd.Cmd):
 
     @docopt_cmd
     def do_todo(self, args):
+        """usage: todo <task_name>"""
         task_name = args["<task_name>"]
         details = input("Enter details: ")
-        print(self.todolist.todo(task_name, details))
+        priority = input("Enter priority: ")
+        print(self.todolist.todo(task_name, details, priority))
 
     @docopt_cmd
     def do_doing(self, args):
+        """usage: doing <task_id> """
         task_id = args["<task_id>"]
         print(self.todolist.doing(task_id))
 
     @docopt_cmd
     def do_done(self, args):
+        """usage: done <task_id>"""
         task_id = args["<task_id>"]
         print(self.todolist.done(task_id))
 
     @docopt_cmd
     def do_edit_task(self, args):
+        """usage: edit_task <task_id>"""
         task_id = args["<task_id>"]
         print(self.todolist.edit_task(task_id))
 
     @docopt_cmd
     def do_delete_task(self, args):
+        """usage: delete_task <task_id>"""
         task_id = args["<task_id>"]
         print(self.todolist.delete_task(task_id))
 
     @docopt_cmd
     def do_list_all(self, _):
+        """usage: list_todo"""
         print(self.todolist.list_all())
 
     @docopt_cmd
-    def do_list_priority(self, _):
-        print(self.todolist.list_priority())
+    def do_list_priority(self, args):
+        """usage: list_priority <priority_level>"""
+        priority_level = args["<priority_level>"]
+        print(self.todolist.list_priority(priority_level))
 
     @docopt_cmd
     def do_list_todo(self, _):
+        """usage: list_todo"""
         print(self.todolist.list_todo())
 
     @docopt_cmd
     def do_list_doing(self, _):
+        """usage: list_doing"""
         print(self.todolist.list_doing())
 
     @docopt_cmd
     def do_list_done(self, _):
+        """usage: list_done"""
         print(self.todolist.list_done())
 
     def do_quit(self, _):
-        print('Todo list closed.')
+        """usage: exit"""
+        print("Todo list closed.")
         exit()
 
 
